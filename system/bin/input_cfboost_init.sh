979f1f116f7759a3b38e05e357c3d9d18cc1c492
tdro <tdro@github.com>
2016-08-15 19:49:38 -0400

    input cpu boost device needs input boost

---
+++ system/bin/input_cfboost_init.sh
@@ -0,0 +1,38 @@
+#!/system/bin/sh
+
+# Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+# input_cfboost_init.sh -- initialize CPU boost parameters
+#
+# This script reads max cpu lp frequency and programs it as default
+# boost frequency for input cfboost kernel module. In case max lp
+# frequency can't be read using sysfs some sane value is set. Default
+# boost time is set to 50ms.
+#
+# Defaults can be override by passing -f and -t options.
+# cfboost: CPU Frequency Booster
+
+READ_MAXLP_FREQ="/sys/devices/system/cpu/cpuquiet/tegra_cpuquiet/idle_top_freq"
+WRITE_BOOST_FREQ="/sys/module/input_cfboost/parameters/boost_freq"
+WRITE_BOOST_TIME="/sys/module/input_cfboost/parameters/boost_time"
+
+# defaults
+FREQ_KHZ=918000
+TIME_MS=500
+
+
+if [ -f $WRITE_BOOST_FREQ ];
+then
+    echo $FREQ_KHZ > $WRITE_BOOST_FREQ
+fi
+
+if [ -f $WRITE_BOOST_TIME ];
+then
+    echo $TIME_MS > $WRITE_BOOST_TIME
+fi
